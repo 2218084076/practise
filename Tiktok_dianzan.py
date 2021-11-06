5aa2e29a625ea83319b869c805968aa50f31a767
Terry <17601306065@163.com>
2021-11-06 15:10:54 +0800

    give the thumbs-up

--- Tiktok_dianzan.py
+++ Tiktok_dianzan.py
@@ -14,7 +14,7 @@ while True:
     get_Tiktok_url(input('抖音分享地址'))
     a = d.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/div[1]/div[3]/div/div[3]')
     b=a.find_element_by_class_name('_314bde61933468933fabb30f1507cdb2-scss').click()'''
-
+key = '努力的打工人'
 share = input('抖音分享地址：')
 share='http'+share.split('http')[1].split('复制')[0]
 d = webdriver.Chrome()
@@ -29,7 +29,7 @@ a=d.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[1]/div[3]/div/div/
 #赞 _314bde61933468933fabb30f1507cdb2-scss fd385a3d6601897cdbce6252a3346600-scss
 #名_52058b306f496907c5d55c0facb81886-scss
 for i in range(0,len(a)+1):
-    if a[i].find_element_by_class_name('_52058b306f496907c5d55c0facb81886-scss').text.split('2021')[0] == '努力的打工人':
+    if a[i].find_element_by_class_name('_52058b306f496907c5d55c0facb81886-scss').text.split('2021')[0] == key:
         a[i].find_element_by_class_name('_314bde61933468933fabb30f1507cdb2-scss').click()
     else:
         print(a[i].find_element_by_class_name('_52058b306f496907c5d55c0facb81886-scss').text)
