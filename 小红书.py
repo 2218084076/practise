dbb696b775408bc0187ab8ca1fda7d134c11aefd
Terry <17601306065@163.com>
2021-11-15 10:07:39 +0800

    update

--- 小红书.py
+++ 小红书.py
@@ -2,7 +2,7 @@ import os
 import time
 import random
 i=1
-l=['869e65410721']
+# l=['869e65410721']
 while i<400:
 	os.system("adb shell input tap 284 1328")
 	os.system("adb shell input swipe 951 994 80 975 100")
