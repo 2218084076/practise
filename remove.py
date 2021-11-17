1cdb3a58a731944a2e46f91dc11744fc3291967e
Terry <17601306065@163.com>
2021-11-17 20:58:22 +0800

    upload

--- remove.py
+++
@@ -1,4 +0,0 @@
-import os
-files = ''
-l1 = os.listdir(r'D:\github\hotpoor_autoclick_xhs\mac_xialiwei_256\local_web\static\files')
-print(l1)
--- 小红书.py
+++ 小红书.py
@@ -2,23 +2,38 @@ import os
 import time
 import random
 i=1
-# l=['869e65410721']
+l=['869e65410721','a0e6c353']
 while i<280:
-	os.system("adb shell input tap 284 1328")
-	os.system("adb shell input swipe 951 994 80 975 100")
-	os.system("adb shell input swipe 951 994 80 975 100")
-	time.sleep(2)
-	os.system("adb shell input tap 71 120")
-	time.sleep(2)
-	os.system("adb shell input tap 800 1414")
-	os.system("adb shell input swipe 951 994 80 975 100")
-	os.system("adb shell input swipe 951 994 80 975 100")
-	time.sleep(1)
-	os.system("adb shell input tap 71 120")
-	time.sleep(1)
-	print(f'n={i}')
+	# t=random.randint(3,8)
+	# os.system("adb shell input tap 284 1328")
+	# os.system("adb shell input swipe 951 994 80 975 100")
+	# os.system("adb shell input swipe 951 994 80 975 100")
+	# time.sleep(2)
+	# os.system("adb shell input tap 71 120")
+	# time.sleep(2)
+	# os.system("adb shell input tap 800 1414")
+	# # os.system("adb shell input swipe 951 994 80 975 100")
+	# # os.system("adb shell input swipe 951 994 80 975 100")
+	# time.sleep(t)
+	# os.system("adb shell input tap 71 120")
+	# time.sleep(1)
+	for n in l:
+		t=random.randint(3,8)
+		os.system(f"adb -s {n} shell input tap 284 1328")
+		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
+		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
+		time.sleep(2)
+		os.system(f"adb -s {n} shell input tap 71 120")
+		time.sleep(2)
+		os.system(f"adb -s {n} shell input tap 800 1414")
+		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
+		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
+		time.sleep(t)
+		os.system(f"adb -s {n} shell input tap 71 120")
+		time.sleep(1)
+	print(f'n={i}',t,'s')
 	i += 1
-
+print("End")
 #adb devices查看连接设备
 #869e65410721 Daniel
 #a022d1760821 frank
