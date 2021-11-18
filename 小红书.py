3cf579ccc5ffa3ef9b9863779358f77df5516296
Terry <17601306065@163.com>
2021-11-18 18:11:51 +0800

    upload

--- 小红书.py
+++ 小红书.py
@@ -2,8 +2,9 @@ import os
 import time
 import random
 i=1
-l=['869e65410721','a0e6c353']
-while i<280:
+l=['869e65410721']
+t=2
+while i<200:
 	# t=random.randint(3,8)
 	# os.system("adb shell input tap 284 1328")
 	# os.system("adb shell input swipe 951 994 80 975 100")
@@ -18,20 +19,22 @@ while i<280:
 	# os.system("adb shell input tap 71 120")
 	# time.sleep(1)
 	for n in l:
-		t=random.randint(3,8)
+		# t=random.randint(1,4)
 		os.system(f"adb -s {n} shell input tap 284 1328")
 		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
 		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
-		time.sleep(2)
+		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
+		# time.sleep(t)
 		os.system(f"adb -s {n} shell input tap 71 120")
-		time.sleep(2)
+		time.sleep(t)
 		os.system(f"adb -s {n} shell input tap 800 1414")
 		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
 		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
-		time.sleep(t)
+		os.system(f"adb -s {n} shell input swipe 951 994 80 975 100")
+		# time.sleep(t)
 		os.system(f"adb -s {n} shell input tap 71 120")
-		time.sleep(1)
-	print(f'n={i}',t,'s')
+		time.sleep(t)
+	print(f'n={i} {t}s')
 	i += 1
 print("End")
 #adb devices查看连接设备
