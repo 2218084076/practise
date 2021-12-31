3629eb0e8b766e592e5ec1bbee4a60f5f9a14cf5
Terry <82017869+2218084076@users.noreply.github.com>
2021-12-31 10:04:50 +0800

    demo

---
+++ course/demo.py
@@ -0,0 +1,26 @@
+## =====> count方法 <====
+# a='Hello World......@13423142'
+# print(f'str(a)为\t"{a}"')
+# print(f'字母H出现的次数\t{a.count("H")}次')
+# print(f'数字3出现的次数\t{a.count("3")}次')
+# print(f'符号"."出现的次数\t{a.count(".")}')
+
+## ====> 计算圆形面积周长 <====
+# import math
+#
+# radius=input("radius of cricle:")
+# radius=int(radius)
+# c = 2*math.pi*radius
+# a=math.pi*radius*radius
+#
+# print ( "circumference of circle(周长): ",c)
+# print ( "area of cricle(面积): ",a)
+
+## ===> 0~50内3的倍数 <===
+# l=[]
+# for i in range(0,51):
+#     if i>0 and i%3 == 0:
+#         # print(i)
+#         l.append(i)
+# print(f'0~50内3的倍数为：{l}')
+
