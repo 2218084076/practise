5034b6ef6fd312fbaca86183586964da35175485
Terry <82017869+2218084076@users.noreply.github.com>
2022-03-12 11:15:51 +0800

    update

---
+++ student/demo220306.py
@@ -0,0 +1,18 @@
+n="Terry"
+p=123
+
+name = str(input("Name:\n"))
+password = int(input("Password:\n"))
+code = "T"
+u_code = str(input("Code:\n"))
+
+if u_code == code:
+	if name == n and password == p:
+		print("Ture")
+	else:
+		print("error")
+else:
+	print("no code")
+
+print("# 在这里编辑")
+# 编辑后要保存
