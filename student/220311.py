c561c27655a6e6618ac61883996a209db2a8596f
Terry <82017869+2218084076@users.noreply.github.com>
2022-03-19 16:06:16 +0800

    update

--- student/220311.py
+++ student/220311.py
@@ -1,1 +1,7 @@
-files={'a':'1','b':'2','c':'1'}
+a={'a':'1','b':'2','c':'1'}
+print(a)
+tmp={}
+for k,v in a.items():
+	print('k:%s,v:%s'%(k,v))
+	tmp.setdefault(v,[]).append(k)
+print(tmp)
--- student/demo220306.py
+++ student/demo220306.py
@@ -1,3 +1,4 @@
+import requests
 n="Terry"
 p=123
 
