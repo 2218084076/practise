94020dcd46ead6864c8121e6226509d8d8c6cd23
Terry <82017869+2218084076@users.noreply.github.com>
2022-06-28 18:51:36 +0800

    添加浏览器无图模式，提高执行效率

--- get_cdf/get_cdf_selenium.py
+++ get_cdf/get_cdf_selenium.py
@@ -1,3 +1,4 @@
+import logging
 import time
 
 import pymongo
@@ -6,41 +7,73 @@ from selenium.webdriver.common.by import By
 
 '''
 
+科颜氏
+https://www.cdfgsanya.com/brand-shop.html?id=248211
+
+阿玛尼
+https://www.cdfgsanya.com/brand-shop.html?id=248148
+
+圣罗兰
+https://www.cdfgsanya.com/brand-shop.html?id=248327
+
+赫莲娜
+https://www.cdfgsanya.com/brand-shop.html?id=248226
+
+碧欧泉
+https://www.cdfgsanya.com/brand-shop.html?id=248153
+
+植村秀
+https://www.cdfgsanya.com/brand-shop.html?id=248431
+
+欧莱雅
+https://www.cdfgsanya.com/brand-shop.html?id=249511
+
+Maison Margiela 
+https://www.cdfgsanya.com/brand-shop.html?id=281265
+
+欧珑
+https://www.cdfgsanya.com/brand-shop.html?id=281031
+
+理肤泉
+https://www.cdfgsanya.com/brand-shop.html?id=248815
+
+薇姿
+https://www.cdfgsanya.com/product-list.html?sw=%E8%96%87%E5%A7%BF
 '''
 
 page_num = 0
 # 起始
-item = 15
+item = 0
+collection_name = 'cdf_220628'
+
+logger = logging.getLogger(__name__)
 
 
 class Browser:
-    url = 'https://www.cdfgsanya.com/brand-shop.html?id=281113'
-    collection_name = 'cdf_ClédePeauBeauté肌肤之钥'
-    options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'eager'
-    # options.headless = True
-    options.add_argument('zh-CN,zh;q=0.9')
-    options.add_argument(
-        'User-Agent= "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-        'Chrome/102.0.5005.115 Safari/537.36"')
-    firefox_options = webdriver.FirefoxOptions()
+    url = 'https://www.cdfgsanya.com/product-list.html?sw=%E8%96%87%E5%A7%BF'
+    collection_name = collection_name
+    prefs = {"profile.managed_default_content_settings.images": 2}
 
     def __init__(self):
         self.db = None
         self.client = None
-        self.mongo_uri = 'localhost:27017'
+        self.mongo_uri = '127.0.0.1:27017'
         self.mongo_db = 'cdf'
-        self.browser = webdriver.Chrome(chrome_options=self.options)
         self.sleep = 4
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("prefs", self.prefs)
+        self.browser = webdriver.Chrome(chrome_options=options)
         self.browser.get(self.url)
 
     def brand_shop(self):
         """
         brand_shop
         """
+        self.browser.minimize_window()
         time.sleep(10)
+
         product_list = self.browser.find_elements(By.CLASS_NAME, 'product-item-default')
-        print('product_list', len(product_list))
+        logger.debug('product_list', len(product_list))
         for p in range(item, len(product_list)):
             product_list[p].click()
 
@@ -51,7 +84,7 @@ class Browser:
             items = len(self.browser.find_elements(By.CLASS_NAME, 'style-normal-item'))
             colors = len(self.browser.find_elements(By.CLASS_NAME, 'style-color-item'))
 
-            print('len(items)', items, 'len(colors)', colors)
+            logger.debug('len(items)', items, 'len(colors)', colors)
 
             if items > 0:
 
@@ -64,7 +97,7 @@ class Browser:
 
                     info_json = self.parse()
                     self.mongo(info_json)
-                    print('page - %s-item %s - info json <%s>' % (p, n, info_json.get('product-name')))
+                    logger.debug('page - %s-item %s - info json <%s>' % (p, n, info_json.get('product-name')))
 
                 self.browser.close()
                 self.browser.switch_to.window((self.browser.window_handles[0]))
@@ -80,7 +113,7 @@ class Browser:
                     time.sleep(self.sleep)
                     info_json = self.parse()
                     self.mongo(info_json)
-                    print('page - %s-item %s - info json <%s>' % (p, c, info_json.get('product-name')))
+                    logger.debug('page - %s-item %s - info json <%s>' % (p, c, info_json.get('product-name')))
 
                 self.browser.close()
                 self.browser.switch_to.window((self.browser.window_handles[0]))
@@ -92,7 +125,7 @@ class Browser:
                 info_json = self.parse()
 
                 self.mongo(info_json)
-                print('page - %s-item - info json <%s>' % (p, info_json.get('product-name')))
+                logger.debug('page - %s-item - info json <%s>' % (p, info_json.get('product-name')))
                 self.browser.close()
                 time.sleep(self.sleep)
                 self.browser.switch_to.window((self.browser.window_handles[0]))  # 切换主页面
@@ -149,15 +182,13 @@ class Browser:
         self.browser.quit()
 
 
-browser = Browser()
-
 if __name__ == '__main__':
+    browser = Browser()
     try:
-        # for p in range(8):
         for page in range(page_num):
             browser.next_page()
         browser.brand_shop()
         browser.close_browser()
     except Exception as e:
-        print('Exception', e)
+        logger.debug('Exception', e)
         browser.close_browser()
