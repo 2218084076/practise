5f159045d41a72de9d602559c09da417dd10d074
Terry <82017869+2218084076@users.noreply.github.com>
2022-07-01 18:57:02 +0800

    需求品牌链接

---
+++ get_cdf/sku_settings.py
@@ -0,0 +1,19 @@
+brand_list = [
+    # 'https://www.cdfgsanya.com/brand-shop.html?id=281031',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248153',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248148',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248226',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248211',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248815',
+    'https://www.cdfgsanya.com/lancome-product-list.html?brandType=-1&brandId=248137&categoryName=%E5%85%B0%E8%94%BB&categoryId=2900',
+    'https://www.cdfgsanya.com/brand-shop.html?id=249511',
+    'https://www.cdfgsanya.com/brand-shop.html?id=281265',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248266',
+    'https://www.cdfgsanya.com/brand-shop.html?id=249144',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248431',
+    'https://www.cdfgsanya.com/brand-shop.html?id=281030',
+    'https://www.cdfgsanya.com/product-list.html?sw=VICHY',
+    'https://www.cdfgsanya.com/brand-shop.html?id=248327',  # YSL美妆
+    'https://www.cdfgsanya.com/brand-shop.html?id=281070',  # YSL眼镜
+    'https://www.cdfgsanya.com/brand-shop.html?id=281092'
+]
